9c484c02d8c713ce9680acd6c0ff8045db06b483
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-20 13:59:03 +0100

    availability on dates works, not the calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { toggleDateInputs } from '../components/booking';
 
 initMapbox();
+toggleDateInputs();
 
 // A BOUGER DANS LE BON FICHIER MAIS JE SAIS PAS LEQUEL...
 $(function () {
--- app/models/terrace.rb
+++ app/models/terrace.rb
@@ -1,8 +1,15 @@
 class Terrace < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 
   has_one_attached :photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
