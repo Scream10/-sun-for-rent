39353947a56c7362994a2124d1f2bdec1b6beac4
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-21 14:33:04 +0100

    calcule total price workiiiiiing

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,8 +1,32 @@
 import flatpickr from 'flatpickr';
 
+// TEST PRIX
+const parseDate = (string) => {
+  const pattern = /(\d{2})-(\d{2})-(\d{4})/;
+  return new Date(string.replace(pattern,'$3-$2-$1'));
+}
+
+const updateCosts = function(days) {
+  const costs = document.querySelector('.costs')
+
+  if (costs) {
+    let daysElem = document.getElementById('days');
+    let totalPriceElem = document.getElementById('total-price');
+
+    let dayRate = JSON.parse(costs.dataset.rate);
+    let price = days * dayRate;
+    let totalPrice = price;
+
+    daysElem.innerHTML = days;
+    totalPriceElem.innerHTML = totalPrice;
+
+  }
+}
+
 const toggleDateInputs = () => {
   const startDateInput = document.getElementById('booking_start_date');
   const endDateInput = document.getElementById('booking_end_date');
+  const costs = document.querySelector('.costs');
 
   if (startDateInput && endDateInput) {
     const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
@@ -14,6 +38,8 @@ const toggleDateInputs = () => {
       autoclose: true,
       onChange: function(selectedDates, selectedDate) {
         if (selectedDate === '') {
+          // test prix
+          costs.classList.remove('is-visible');
           endDateInput.disabled = true;
         }
         let minDate = selectedDates[0];
@@ -26,7 +52,20 @@ const toggleDateInputs = () => {
       flatpickr(endDateInput, {
         dateFormat: 'd-m-Y',
         disable: unvailableDates,
-        autoclose: true
+        autoclose: true,
+        onChange: function(_, selectedDate) {
+          if (selectedDate === '') {
+            costs.classList.remove('is-visible');
+          } else {
+            let startDate = parseDate(startDateInput.value);
+            let endDate = parseDate(endDateInput.value)
+            // TEST PRIX
+            let days = Math.ceil((endDate - startDate) / (1000 * 3600 * 24));
+            updateCosts(days);
+
+            costs.classList.add('is-visible');
+          }
+        },
       });
   }
 };
